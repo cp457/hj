0c90e5140f85801e5596db2e5029e72c7168b4ef
[Chris] <[christianpriolo96@gmail.com]>
2022-12-20 13:43:30 +0100

    VaribleString

---
+++ src/main/kotlin/Min.kt
@@ -0,0 +1,11 @@
+class Main {
+
+}
+    fun main() {
+
+        val f1: Int = 5
+        val f2: Double = 3.4
+        val f3: Short = 1
+
+        println("f1 is ${f1}, f2 is ${f2}, f3 is ${f3}")
+}
